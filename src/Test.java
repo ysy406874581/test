81e7ebdf70317effee4ee15a223be47db3577d4a
ysy406874581 <yang406874581@163.com>
2017-05-26 18:22:16 +0800

    Update Test.java

--- src/Test.java
+++ src/Test.java
@@ -3,18 +3,6 @@
 public class Test {
 
     public static void main(String[] args) {
-
-
-yyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyy
-
-热鹅鹅鹅鹅鹅鹅鹅鹅鹅鹅鹅鹅鹅鹅鹅饿饿
-	222
-11111111111111111111
-
-
-yyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyw1111111111111111111111111111111111111111111111
-wqqwwq
-	22222222222222222222222222222222222
         System.out.println("212121212121");
     }
 }
