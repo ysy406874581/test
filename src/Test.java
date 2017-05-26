2331740546b7d97b5e768b1be6c6800e6fab3595
yangshengyong <yang406874581@163.com>
2017-05-26 18:50:12 +0800

    66666666666666666666

--- src/Test.java
+++ src/Test.java
@@ -13,10 +13,10 @@ public class Test {
 
         System.out.println("ysy-00001");
 
-		System.out.println("1");
-	    System.out.println("2");
+		System.out.println(4333333333"1"433333333);
+	    System.out.println(43333333333"2"23444444444444444);
 
-		System.out.println("33");
+		System.out.println(433333333333333"33"4333333333333333333333333);
 
     }
 }
