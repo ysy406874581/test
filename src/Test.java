6884e25ba2bf1d279815c65d97eeeea9211ad081
ysy406874581 <yang406874581@163.com>
2017-05-26 18:05:02 +0800

    Update Test.java

--- src/Test.java
+++ src/Test.java
@@ -4,11 +4,6 @@ public class Test {
 
     public static void main(String[] args) {
 
-
-额外无无无无无无无无无无无无无无无无无无无无无无无
-热鹅鹅鹅鹅鹅鹅鹅鹅鹅鹅鹅鹅鹅鹅鹅饿饿
-11111111111111111111
-
         System.out.println("212121212121");
     }
 }
